052558083715ace8b6251e00c0d1cc0ce59978c6
Csáki Márk <csakimark2000@gmail.com>
2023-05-24 17:38:17 +0200

    Lock and Key IV
    Mukodokepes kulcsra nyithato ladak es ajtok.

---
+++ Assets/Scripts/Doors/Kulcsos_ajtok.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kulcsos_ajtok : MonoBehaviour
+{
+    private Animator animator;
+
+    private bool animationplayed;
+
+    public Light doorLight;
+
+    public GameObject lakat;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animationplayed = false;
+        if (doorLight != null)
+        {
+            doorLight.enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lakat==null &&!animationplayed)
+        {
+            animator.SetTrigger("OpenDoor");
+            animationplayed = true;
+            if (doorLight != null)
+            {
+                doorLight.enabled = true;
+            }
+        }
+    }
+}
